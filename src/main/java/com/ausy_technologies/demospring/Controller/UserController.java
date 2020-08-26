64884b441c6bb66f2327dfb1fbc7ab68197c9906
MikeDerWolf <mihai.lupu777@gmail.com>
2020-08-26 13:38:53 +0300

    First commit

--- src/main/java/com/ausy_technologies/demospring/Controller/UserController.java
+++ src/main/java/com/ausy_technologies/demospring/Controller/UserController.java
@@ -13,15 +13,16 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserService userService2;
 
+    //Nice work
 
 
     @PostMapping("/addRole")
     public Role saveRole(@RequestBody Role role) {
 
 
-        Role roleAdded = this.userService.saveRole(role);
+        Role roleAdded = this.userService2.saveRole(role);
         return roleAdded;
     }
 
@@ -30,46 +31,46 @@ public class UserController {
 
     @PostMapping("/addUser")
     public User saveUser(@RequestBody User user) {
-        User userAdded = this.userService.saveUser(user);
+        User userAdded = this.userService2.saveUser(user);
         return userAdded;
     }
 
     @PostMapping("/addUser2/{idRole}")
     public User saveUser2(@RequestBody User user, @PathVariable int idRole)
     {
-        return this.userService.saveUser2(user,idRole);
+        return this.userService2.saveUser2(user,idRole);
 
     }
 
     @PostMapping("/addUser3/{roleList}")
     public User saveUser3(@RequestBody User user , @PathVariable List<Role> roleList)
     {
-        return this.userService.saveUser3(user,roleList);
+        return this.userService2.saveUser3(user,roleList);
     }
 
     @GetMapping("/findRoleBy/{id}")
     public Role findRoleById(@PathVariable int id)
     {
-  return this.userService.findRoleById(id);
+  return this.userService2.findRoleById(id);
     }
 
     @GetMapping("/findAllRoles")
     public List<Role> findAllRoles()
     {
-        return  userService.findAllRoles();
+        return  userService2.findAllRoles();
     }
 
 
     @GetMapping("/allUsers")
     public List<User> findAllUsers()
     {
-        return this.userService.findAllUsers();
+        return this.userService2.findAllUsers();
     }
 
     @DeleteMapping("/deleteUserById/{id}")
     public void deleteUser(@PathVariable int id)
     {
-        this.userService.deleteUserById(id);
+        this.userService2.deleteUserById(id);
 
     }
 
